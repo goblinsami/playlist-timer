d426ead0265c2b500ddc4beba49a7ad2012b5aa2
goblinsami <goblinsami@gmail.com>
2026-06-26 21:54:44 +0200

    feat: add Google Tag Manager scripts to enhance tracking capabilities

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,9 +9,20 @@ export default defineNuxtConfig({
       link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
       script: [
         {
+          key: 'google-tag-manager',
+          innerHTML: "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':\nnew Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],\nj=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=\n'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);\n})(window,document,'script','dataLayer','GTM-NNPTGT28');",
+        },
+        {
           async: true,
           src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8445882609109686',
           crossorigin: 'anonymous',
+        },
+      ],
+      noscript: [
+        {
+          key: 'google-tag-manager-noscript',
+          innerHTML: '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NNPTGT28" height="0" width="0" style="display:none;visibility:hidden"></iframe>',
+          tagPosition: 'bodyOpen',
         },
       ],
     },
